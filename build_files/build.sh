75241e93e065778342f37fc943924534abe69ee6
Zachary Hall <catmeow72@proton.me>
2025-07-30 08:11:28 -0700

    Fix incorrect pipx package name

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y git rsync sddm pipz
+dnf5 install -y git rsync sddm pipx
 pipx install pipenv
 mkdir -p /var/cache/{src,build,logs}
 
